f8afeca6b68b584b7ad7bc4ef4c5822db5311851
user <user@69e63612-969a-4371-9866-b14aae95a0cb>
2011-07-05 01:03:01 +0000

    Modifications module org_lucterios_updates (1.2.4.85)
    git-svn-id: http://projets.lucterios.org/svn/extensions/org_lucterios_updates@1215 69e63612-969a-4371-9866-b14aae95a0cb

---
+++ extensionNotify.inc.php
@@ -0,0 +1,32 @@
+<?php
+// 	This file is part of Diacamma, a software developped by "Le Sanglier du Libre" (http://www.sd-libre.fr)
+// 	Thanks to have payed a retribution for using this module.
+// 
+// 	Diacamma is free software; you can redistribute it and/or modify
+// 	it under the terms of the GNU General Public License as published by
+// 	the Free Software Foundation; either version 2 of the License, or
+// 	(at your option) any later version.
+// 
+// 	Diacamma is distributed in the hope that it will be useful,
+// 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+// 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// 	GNU General Public License for more details.
+// 
+// 	You should have received a copy of the GNU General Public License
+// 	along with Lucterios; if not, write to the Free Software
+// 	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+// 
+// 		Contributeurs: Fanny ALLEAUME, Pierre-Olivier VERSCHOORE, Laurent GAY
+// library file write by SDK tool
+// --- Last modification: Date 04 July 2011 22:50:42 By  ---
+
+//@BEGIN@
+function org_lucterios_updates_extensionNotify($value) {
+	if ($value==0) {
+		require_once("extensions/org_lucterios_updates/UpdateServers.tbl.php");
+		$UpdateServer=new DBObj_org_lucterios_updates_UpdateServers;
+		$UpdateServer->clearDateUpdate();
+	}
+}
+//@END@
+?>
