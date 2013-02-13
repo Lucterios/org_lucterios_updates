483a808c3e16ad6cc8ee4aeb776e3fffce8a6a4c
Laurent GAY <l.gay@sd-libre.fr>
2013-02-13 21:51:00 +0100

    Asso v1.7 - Syndix v1.1

--- setup.inc.php
+++ setup.inc.php
@@ -27,10 +27,10 @@ $extension_libre=true;
 $version_max=1;
 $version_min=4;
 $version_release=4;
-$version_build=122;
+$version_build=123;
 
 $depencies=array();
-$depencies[0] = new Param_Depencies("CORE", 1, 4, 1, 3, false);
+$depencies[0] = new Param_Depencies("CORE", 1, 5, 1, 4, false);
 
 $rights=array();
 $rights[1] = new Param_Rigth("Paramètrage",85);
