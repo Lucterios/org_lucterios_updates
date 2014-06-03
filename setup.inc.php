2d6ca246c9ee01edbd14f9c1d317fc6b656bcf24
support <support@sd-libre.fr>
2014-06-03 11:28:57 +0000

    CORE 1.6

--- setup.inc.php
+++ setup.inc.php
@@ -1,20 +1,4 @@
 <?php
-// This file is part of Lucterios/Diacamma, a software developped by 'Le Sanglier du Libre' (http://www.sd-libre.fr)
-// thanks to have payed a retribution for using this module.
-// 
-// Lucterios/Diacamma is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-// 
-// Lucterios/Diacamma is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// along with Lucterios; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 // setup file write by Lucterios SDK tool
 
 $extention_name="org_lucterios_updates";
@@ -25,12 +9,12 @@ $extention_titre="Mise à jour & installation de modules";
 $extension_libre=true;
 
 $version_max=1;
-$version_min=4;
-$version_release=5;
-$version_build=138;
+$version_min=5;
+$version_release=1;
+$version_build=139;
 
 $depencies=array();
-$depencies[0] = new Param_Depencies("CORE", 1, 5, 1, 4, false);
+$depencies[0] = new Param_Depencies("CORE", 1, 6, 1, 4, false);
 
 $rights=array();
 $rights[1] = new Param_Rigth("Paramètrage",85);
